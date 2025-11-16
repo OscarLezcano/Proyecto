d505aab1467f58019d7087d985b9a6f51885b484
OscarLezcano <oscar.lezcano@fiuni.edu.py>
2025-11-15 22:43:39 -0300

    Fix ya funciona TablaProducto
    Cambie de tsx a jsx porque se me canto de los
    wewos

---
+++ src/components/TablaProducto.jsx
@@ -0,0 +1,40 @@
+//import { JSX } from "react";
+import { useEffect, useState } from "react";
+import Api from "../api/Api";
+
+function TablaProducto() {
+	const [productos, setProductos] = useState([]);
+
+	useEffect(() => {
+		Api.getProductos().then((p) => setProductos(p || []));
+	}, []);
+
+	const rows = [];
+	for (const producto of productos) {
+		rows.push(
+			<tr>
+				<td>{producto.id}</td>
+				<td>{producto.name}</td>
+				<td>{producto.price}</td>
+				<td>{producto.stock_quantity}</td>
+				<td>NADA</td>
+			</tr>
+		);
+	}
+	return (
+		<table>
+			<thead>
+				<tr>
+					<th>ID</th>
+					<th>NOMBRE</th>
+					<th>PRECIO</th>
+					<th>STOCK</th>
+					<th>ACCION</th>
+				</tr>
+			</thead>
+			<tbody>{rows}</tbody>
+		</table>
+	);
+}
+
+export default TablaProducto;
